88bba8e3d8a9880ca1c93ede333298ae63192ffa
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-26 23:04:34 +0300

    Set up env for the project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-anonymous-default-export */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}","./public/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:"hsl(1, 90%, 64%)"
+      },
+      fontFamily:{
+        jakarta :"Plus Jakarta Sans"
+      },
+      fontWeight:{
+        jakarta500:"500",
+        jakarta800: "800"
+      }
+    },
+  },
+  plugins: [],
+}
+
