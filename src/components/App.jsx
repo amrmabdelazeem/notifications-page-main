ba0004826f51720550d374dc4dad8f04638836bd
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-28 01:17:31 +0300

    Set logic for the decrease counter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import Notification from "./Notification.jsx";
 
 export default function App() {
+const [isRead, setIsRead] = useState(true)
+const [notifications, setNotifications] = useState(3);
+
+const markAllRead = () =>{
+  setIsRead(!isRead);
+  setNotifications(0)
+}
+
   return (
     <div className="container py-5 px-4 font-jakarta">
       <div className="header flex justify-between">
         <h1 className=" text-black font-jakarta800 text-base">
-          Notifications{" "}
+          Notifications
           <span className=" text-white bg-secondary py-0 px-2 rounded-md">
-            3
+            {notifications}
           </span>
         </h1>
-        <button className=" text-gray-400 text-xs">Mark all as read</button>
+        <button onClick={markAllRead} className=" text-gray-400 text-xs">Mark all as read</button>
       </div>
       <div className="notifications pt-5">
-      <Notification avatar="mark-webber" userName="Mark Webber" action="react to your recent post" achievement="My first tournament today!" timestamp="1m ago" read={true}/>
-      <Notification avatar="angela-gray" userName="Angela Gray" action="followed you" achievement="" timestamp="5m ago" read={true} />
-      <Notification avatar="jacob-thompson" userName="Jacob Thompson" action="has joined your group" achievement="Chess Club" timestamp="1 day ago" read={true}/>
-      <Notification avatar="rizky-hasanuddin" userName="Rizky Hasanuddin" action="sent you a private message" achievement="" timestamp="5 days ago" privateMsg = {true} read={false}/>
-      <Notification avatar="kimberly-smith" userName="Kimberly Smith" action="commented on your picture" achievement="" timestamp="1 week ago" read={false} picture={true}/>
-      <Notification avatar="nathan-peterson" userName="Nathan Peterson" action="reacted to your recent post" achievement="5 end-game strategies to increase your win rate" timestamp="2 weeks ago" read={false}/>
-      <Notification avatar="anna-kim" userName="Anna Kim" action="left the group" achievement="Chess Club" timestamp="2 weeks ago" read={false}/>
+        <Notification
+          id={1}
+          avatar="mark-webber"
+          userName="Mark Webber"
+          action="react to your recent post"
+          achievement="My first tournament today!"
+          timestamp="1m ago"
+          read={isRead}
+        />
+        <Notification
+          id={2}
+          // onclicked={(isRead)=> handleCardClicks(2, isRead)}
+          avatar="angela-gray"
+          userName="Angela Gray"
+          action="followed you"
+          achievement=""
+          timestamp="5m ago"
+          read={isRead}
+        />
+        <Notification
+          id={3}
+          // onclicked={(isRead)=> handleCardClicks(3, isRead)}
+          avatar="jacob-thompson"
+          userName="Jacob Thompson"
+          action="has joined your group"
+          achievement="Chess Club"
+          timestamp="1 day ago"
+          read={isRead}
+        />
+        <Notification
+          id={4}
+          // onclicked={(isRead)=> handleCardClicks(4, isRead)}
+          avatar="rizky-hasanuddin"
+          userName="Rizky Hasanuddin"
+          action="sent you a private message"
+          achievement=""
+          timestamp="5 days ago"
+          privateMsg={true}
+          // read={false}
+        />
+        <Notification
+          id={5}
+          // onclicked={(isRead)=> handleCardClicks(5, isRead)}
+          avatar="kimberly-smith"
+          userName="Kimberly Smith"
+          action="commented on your picture"
+          achievement=""
+          timestamp="1 week ago"
+          // read={false}
+          picture={true}
+        />
+        <Notification
+          id={6}
+          // onclicked={(isRead)=> handleCardClicks(6, isRead)}
+          avatar="nathan-peterson"
+          userName="Nathan Peterson"
+          action="reacted to your recent post"
+          achievement="5 end-game strategies to increase your win rate"
+          timestamp="2 weeks ago"
+          // read={false}
+        />
+        <Notification
+          id={7}
+          // onclicked={(isRead)=> handleCardClicks(7, isRead)}
+          avatar="anna-kim"
+          userName="Anna Kim"
+          action="left the group"
+          achievement="Chess Club"
+          timestamp="2 weeks ago"
+          // read={false}
+        />
       </div>
     </div>
   );
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,16 +1,13 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export default function Notification(props) {
-    const [colorState , setColorState] = useState(true);
-
-    
-    const handleClick = () =>{
-        setColorState(prevColor =>{
-              return !prevColor;
-        })
-    }
+  
   return (
-    <div onClick={handleClick} className={`card flex rounded-md gap-2 p-3 mb-2 bg-opacity-10 cursor-pointer ${colorState === props.read ? "bg-blue-300" : "bg-transparent" }`}>
+    <div
+      className={`card flex rounded-md gap-2 p-3 mb-2 bg-opacity-10 cursor-pointer ${
+        props.read ? "bg-blue-300" : "bg-transparent"
+      }`}
+    >
       <img
         className="w-8 h-8"
         src={`./images/avatar-${props.avatar}.webp`}
@@ -29,10 +26,18 @@ export default function Notification(props) {
           >
             {props.achievement}
           </span>
-          <div className={`ml-2 inline absolute bottom-1.5 rounded w-2 h-2 ${colorState !== props.read ? "bg-transparent" : "bg-primary"}`}></div>
+          <div
+            className={`ml-2 inline absolute bottom-1.5 rounded w-2 h-2 ${
+              props.read ? "bg-primary" : "bg-transparent"
+            }`}
+          ></div>
         </span>
         <p className="text-xs  mt-1 text-gray-400">{props.timestamp}</p>
-        <div className={`${props.privateMsg ? "block" : "hidden"} text-gray-500 border-2 rounded-md text-xs py-4 px-6 mt-3`}>
+        <div
+          className={`${
+            props.privateMsg ? "block" : "hidden"
+          } text-gray-500 border-2 rounded-md text-xs py-4 px-6 mt-3`}
+        >
           <p>
             Hello, thanks for setting up the Chess Club. I've been a member for
             a few weeks now and I'm already having lots of fun and improving my
@@ -40,9 +45,13 @@ export default function Notification(props) {
           </p>
         </div>
       </div>
-      <div className={`picture ${props.picture ? "" : "hidden"}  w-16  flex justify-end`}>
-            <img className="w-7 h-7" src="./images/image-chess.webp" alt="chess" />
-        </div>
+      <div
+        className={`picture ${
+          props.picture ? "" : "hidden"
+        }  w-16  flex justify-end`}
+      >
+        <img className="w-7 h-7" src="./images/image-chess.webp" alt="chess" />
+      </div>
     </div>
   );
 }
